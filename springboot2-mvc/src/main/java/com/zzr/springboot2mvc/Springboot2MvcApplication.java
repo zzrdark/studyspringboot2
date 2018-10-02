6eb06fed1a8e0ec17209a347e30f992d5ba29ba0
zhiruizhang <zzrdark@aliyun.com>
2018-10-02 22:13:50 +0800

    Init commit

---
+++ springboot2-mvc/src/main/java/com/zzr/springboot2mvc/Springboot2MvcApplication.java
@@ -0,0 +1,12 @@
+package com.zzr.springboot2mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2MvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2MvcApplication.class, args);
+    }
+}
